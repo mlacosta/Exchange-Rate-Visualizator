b80cc1560996ed1f0d45f27552718862836c3b03
mlacosta <marianoacosta.003@gmail.com>
2020-06-09 18:29:34 -0300

    fetch functional

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -1,19 +1,19 @@
 const apiKey = 'RSY8066JCSOQEQU8';
 const functionType =  'CURRENCY_EXCHANGE_RATE';
 const fromCurrency = 'from_currency=USD';
-const toCurrency = 'to_currency=JPY';
+const toCurrency = 'to_currency=USD';
 const url = 'https://www.alphavantage.co/query?';
 const fetch = require("node-fetch");
 
-const getExchange = async () =>{
+async function getExchange() {
     const endpoint = `${url}function=${functionType}&${fromCurrency}&${toCurrency}&apikey=${apiKey}`;
     
     try{
         const response = await fetch(endpoint);
         if (response.ok){
             const jsonResponse = await response.json();
-
-
+            console.log(jsonResponse)
+            //console.log(jsonResponse['Realtime Currency Exchange Rate'])
         }
     }
     catch(error){
@@ -25,5 +25,5 @@ const getExchange = async () =>{
 }
 
 
-const data = getExchange();
+getExchange();
 
